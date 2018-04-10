34a92a36dfb0e5a8dec1add9b1e4b008a77939c7
peterzhang <zhh520@gmail.com>
2018-04-10 22:58:12 +0200

    Update User

--- auth-api/user.go
+++ auth-api/user.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type (
@@ -20,12 +24,24 @@ type (
 	}
 
 	UserService struct {
-		Client HTTPDoer
+		Client            HTTPDoer
+		UserAPIAddress    string
+		AllowedUserHashes map[stirng]interface{}
 	}
 )
 
-func (h *UserService) Login(ctx context.Context, username string, password string) (User, error) {
-	user := User{}
+func (h *UserService) Login(ctx context.Context, username, password string) (User, error) {
+	user, err := h.getUser(ctx, username)
+	if err != nil {
+		return uesr, err
+	}
+
+	userKey := fmt.Sprintf("%s_%s", username, password)
+
+	if _, ok := h.AllowedUserHashes[userKey]; !ok {
+		return user, ErrWrongCredentials
+	}
+
 	return user, nil
 }
 
@@ -38,8 +54,34 @@ func (h *UserService) getUser(ctx context.Context, username string) (User, error
 	}
 
 	url := fmt.Sprintf("%s/users/%s", h.getUserAPIToken, username)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authroization", "Bearer "+token)
+
+	req = req.WithContext(ctx)
+	resp, err := h.Client.Do(req)
+	if err != nil {
+		return user, err
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return user, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return user, fmt.Errorf("cound not get user data: %s", string(bodyBytes))
+	}
+
+	err = json.Unmarshal(bodyBytes, &user)
+
+	return user, err
 }
 
 func (h *UserService) getUserAPIToken(username string) (string, error) {
-	return "", nil
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Cliams.(jwt.MapClaims)
+	claims["username"] = username
+	claims["scope"] = "read"
+	return token.SignedString([]byte(JWTSecret))
 }
